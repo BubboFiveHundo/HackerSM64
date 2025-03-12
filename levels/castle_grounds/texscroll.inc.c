e12c53f17ddd50b0d723ccab4a2b3fb4e42ce770
BubboFiveHundo <brycebucher39@gmail.com>
2025-03-12 02:52:03 -0500

    a

--- levels/castle_grounds/texscroll.inc.c
+++ levels/castle_grounds/texscroll.inc.c
@@ -27,6 +27,36 @@ void scroll_castle_grounds_dl_WaterMesh_mesh_layer_5_vtx_0() {
 	currentX += deltaX;	currentY += deltaY;
 }
 
+void scroll_castle_grounds_dl_Cylinder_005_mesh_layer_1_vtx_4() {
+	int i = 0;
+	int count = 18;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_Cylinder_005_mesh_layer_1_vtx_4);
+
+	deltaX = (int)(0.009999999776482582 * 0x20) % width;
+	deltaY = (int)(0.019999999552965164 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;	currentY += deltaY;
+}
+
 void scroll_castle_grounds() {
 	scroll_castle_grounds_dl_WaterMesh_mesh_layer_5_vtx_0();
+	scroll_castle_grounds_dl_Cylinder_005_mesh_layer_1_vtx_4();
 };
--- levels/castle_grounds/texscroll.inc.h
+++ levels/castle_grounds/texscroll.inc.h
@@ -1,2 +1,3 @@
 extern void scroll_castle_grounds_dl_WaterMesh_mesh_layer_5_vtx_0();
+extern void scroll_castle_grounds_dl_Cylinder_005_mesh_layer_1_vtx_4();
 extern void scroll_castle_grounds();
